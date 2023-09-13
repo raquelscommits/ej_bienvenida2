f5781ec3d7d2e8fbab81555ce34e6953f8e84f87
Raquel <raquelsweeta@gmail.com>
2023-09-13 11:26:58 +0200

    ✨ added button to return to main page in carousel page

--- src/components/CarouselPage.tsx
+++ src/components/CarouselPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import '.././App.css';
 
 const CarouselPage: React.FC = () => {
@@ -13,12 +14,17 @@ const CarouselPage: React.FC = () => {
     setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
   };
 
-  return (
-    <div className="gallery-container">
-      <div className="gallery">
-        <button className="nav-button" id="prevButton" onClick={goToPrevious}>←</button>
-        <img src={images[currentIndex]} alt={`Serie ${currentIndex + 1}`} className="gallery-img" />
-        <button className="nav-button" id="nextButton" onClick={goToNext}>→</button>
+return (
+    <div>
+      <div className="gallery-container">
+        <div className="gallery">
+          <button className="nav-button" id="prevButton" onClick={goToPrevious}>←</button>
+          <img src={images[currentIndex]} alt={`Serie ${currentIndex + 1}`} className="gallery-img"/>
+          <button className="nav-button" id="nextButton" onClick={goToNext}>→</button>
+          <div className="return-button">
+            <Link to="/">Volver a la página principal</Link>
+          </div>
+        </div>
       </div>
     </div>
   );
